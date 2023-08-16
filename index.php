3b17279dba96546ee224eca155c09cee2cad4bd6
qmsteven <74738665+qmsteven@users.noreply.github.com>
2023-08-16 15:52:40 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    $title = "Quinn's Website";
+    $active = 'home';
+    include 'header.php';
+?>
+
+<h1><?php echo $title ?></h1>
+<p>Hi, my name is Quinlan Stevenson! I am a fourth year student at <a href="https://lakeheadu.ca/about/orillia-campus">Lakehead University</a>.</p>
+<h2>About me:</h2>
+<div class="container" style="width: 100%">
+    <div class="text">
+        <p>I grew up in a small town in southwestern Ontario called Stratford. I was born in the Stratford General Hospital but lived in Mitchell Ontario for the first 6 years of my life. I was enrolled in pre-kindergarten at the Perth Care For Kids and junior kindergarten at Upper Thames Elementary School. I then moved to Stratford to attend Bedford Public Elementary School for my senior kindergarten year. I was enrolled in French Immersion courses once I got into grade one and continued until I graduated from grade 12. After completing grade 6, I moved schools to go to Stratford Central Elementary School in 2012. I was there until I completed grade 8 in 2014. I then moved to Stratford Central Secondary School for grades 9 through 12, where I graduated in 2018, but decided to take a fifth year to hone my skills in Computer Science.</p>
+    </div>
+    <div class="image">
+        <img src="/images/stratford-city-hall.jpg" alt="Stratford City Hall" />
+    </div>
+</div>
+<div class="container" style="width: 100%">
+    <div class="image">
+        <img src="/images/georgian-barrie.jpeg" alt="Georgian College" />
+    </div>
+    <div class="text" style="padding-left: 10px;">
+        <p>I started at Georgian College in 2020, I learned front-end and app development. I learned how to code apps in Kotlin for Android devices and C#, C, C++, .NET and NodeJS for web development. This web app is coded in NodeJS! I learned a lot about what it means to be independent in an environment that was crippled by the effects of the global COVID-19 pandemic. I met new friends, with much difficulty, and learned a lot of skills that will help me for the rest of my life.</p>
+</div>
+</div>
+<div class="container" style="width: 100%">
+    <div class="text" style="align-text: right;">
+        <p>In 2022, I moved to Orillia and started my two years at Lakehead University's campus in Orillia. I learned about for software structures and how to use stacks and arrays. I loved the campus but the teachers left a lot to be desired. I worked a lot on some independent projects that furthered my learning about what I loved doing, creating websites and even began making websites for people!</p>
+    </div>
+    <div class="image">
+        <img src="/images/lakehead-orillia.jpeg" alt="Lakehead University" style="float: right;"/>
+    </div>
+</div>
+<p>This is where my story ends, for now. Please follow along with me in this crazy adventure we call life!</p>
+
+<?php
+    include 'footer.php';
+?>
