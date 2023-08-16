da4f27dc6a1c46d8a62399a6f011ec305bde1ab9
qmsteven <74738665+qmsteven@users.noreply.github.com>
2023-08-16 15:53:40 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+</section>
+		<footer>
+			<p style="margin: 0;">Quinlan Stevenson | <a href="mailto:quinlan.m.stevenson@gmail.com">quinlan.m.stevenson@gmail.com</a></p>
+		</footer>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+	</body>
+</html>
